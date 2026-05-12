96e2ba52090a2ce3221c2efa416ae0131a620873
sakshi-builds <sharmasakshi1117@gmail.com>
2026-05-12 23:12:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let sel=document.getElementById("colorSelect");
+ function removeColor(){
+	 sel.remove(sel.selectedIndex);
+ }
+let x=document.getElementsByTagName("input");
+x.addEventListener("click",removeColor);
